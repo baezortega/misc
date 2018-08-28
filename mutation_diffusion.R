1edd491f0e1fbdc1de8f86d96d437e1daf7ccfb0
baezortega <adrianbaez91@gmail.com>
2018-08-28 17:08:12 +0100

    minor change

--- mutation_diffusion.R
+++ mutation_diffusion.R
@@ -5,26 +5,26 @@
 
 # SIMULATION PARAMETERS
 # Population sizes:                    N = [1000, 2000, ..., 99000, 100000]   (100 elements)
-# Target allele frequencies:          fT = [1e-3, ..., 9e-3, 1e-2, ..., 9e-2] (18 elements)
+# Target allele frequencies:          Ft = [1e-3, ..., 9e-3, 1e-2, ..., 9e-2] (18 elements)
 # Initial allele count:               c0 = 1                                  (f0 = 1 / (2N))
-# Simulations per choice of N and fT:  M = 1000
+# Simulations per choice of N and ft:  M = 1000
 
 Ns = seq(1e3, 1e5, 1e3)
-FTs = as.numeric(outer(1:9, 10^(-3:-2)))
+Fts = as.numeric(outer(1:9, 10^(-3:-2)))
 C0 = 1
 M = 1000
 
 
 # Initialise output object
-# For each target frequency fT and population size N, we simulate M independent populations
+# For each target frequency ft and population size N, we simulate M independent populations
 # Matrix g.diff (dim MxN) will store the number of generations until successful diffusion
 # Matrix g.loss (dim MxN) will store the number of generations until loss (up to M losses)
 # Vector n.loss (length N) will store the number of mutation losses for each N
 
-mut.diffusion = structure(vector(mode="list", length=length(FTs)),
-                          names=paste0("fT=", FTs))
+mut.diffusion = structure(vector(mode="list", length=length(Fts)),
+                          names=paste0("ft=", Fts))
 
-for (i in seq(FTs)) {
+for (i in seq(Fts)) {
     
     mut.diffusion[[i]] = list("g.diff" = matrix(NA, nrow=M, ncol=length(Ns),
                                                 dimnames=list(NULL, paste0("N=", Ns))),
@@ -36,11 +36,11 @@ for (i in seq(FTs)) {
 }
 
 
-# For each target allele frequency fT
-for (i in seq(FTs)) {
+# For each target allele frequency ft
+for (i in seq(Fts)) {
     
     print(names(mut.diffusion)[i])
-    fT = FTs[i]
+    ft = Fts[i]
     
     # For each population size (N)
     for (j in seq(Ns)) {
@@ -48,14 +48,14 @@ for (i in seq(FTs)) {
         print(names(mut.diffusion[[i]]$n.loss)[j])
         N = Ns[j]
         
-        # Simulate M mutations for current fT and N
+        # Simulate M mutations for current ft and N
         for (k in 1:M) {
             
             # Introduce mutation: generation = 0, frequency = 1/(2N)
             g = 0
             f = C0 / (2*N)
             
-            while (f < fT) {
+            while (f < ft) {
                 
                 # Draw number of allele copies in the next generation:
                 # c(g+1) ~ Binomial(2N, f(g) = c(g)/(2N))
@@ -115,12 +115,12 @@ plot3d = function(x, y, main) {
 }
 
 
-# a) Plot distribution of the diffusion time for fT=0.01
+# a) Plot distribution of the diffusion time for ft=0.01
 # First, we convert the values in `g.diff` to the 'long format'
 Ns = seq(1e3, 1e5, by=1e3)
 M = 1000
 g.diff.long = cbind("N"=rep(Ns, each=M),
-                    "generations"=as.numeric(mut.diffusion$`fT=0.01`$g.diff))
+                    "generations"=as.numeric(mut.diffusion$`ft=0.01`$g.diff))
 
 # Plot distribution
 plot3d(x=g.diff.long[,1],
@@ -128,10 +128,10 @@ plot3d(x=g.diff.long[,1],
        main=bquote(italic("f")["T"] ~ "=" ~ 0.01))
 
 
-# b) Plot distribution of the time to mutation loss for fT=0.05
+# b) Plot distribution of the time to mutation loss for ft=0.05
 # Convert the values in `g.loss` to the 'long format'
 g.loss.long = cbind("N"=rep(Ns, each=M),
-                    "generations"=as.numeric(mut.diffusion$`fT=0.05`$g.loss))
+                    "generations"=as.numeric(mut.diffusion$`ft=0.05`$g.loss))
 
 # Plot distribution
 plot3d(x=g.loss.long[,1],
